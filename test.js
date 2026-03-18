4508cd1dd677bd0a0f28453e67f2a3e79f7a1586
W4sN0tM3 <shahadalbidar34@gmail.com>
2026-03-17 23:52:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-fetch('https://discord.com/api/webhooks/1483575057572823102/12TVS8JFAVTb_M1Chk-hje4cL5FDMB_niRX80EWdiE0DnnwQGVmLxXjX_REQxH_FopZS', {
+fetch('https://discord.com/api/webhooks/1483674310894686218/4vfFs_gCMSHonr7AGWKf4yqgoE9DSuaTot7XhXemDE6o5b8GQWraBbk7qbY5S9G2VDST', {
   method: 'POST',
   headers: {'Content-Type': 'application/json'},
   body: JSON.stringify({content: 'TEST from console'})
